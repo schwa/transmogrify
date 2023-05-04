c8898def199b0eebe3a4282a8f6ba602f7682229
Jonathan Wight <jwight@mac.com>
2023-05-04 10:13:11 -0700

    Simplify converters by using direct files.

--- transmogrify/main.py
+++ transmogrify/main.py
@@ -7,6 +7,8 @@ import json
 import toml
 import yaml
 import sys
+import io
+
 
 app = typer.Typer()
 
@@ -31,72 +33,47 @@ class Converter:
     def __init__(self):
         pass
 
-    def load(self, path: Path) -> object:
+    def load(self, file: io.IOBase) -> object:
         pass
 
-    def dump(self, path: Path, data: object):
+    def dump(self, file: io.IOBase, data: object):
         pass
 
     @classmethod
     def converter(cls, format: Format):
-        if format == Format.csv:
-            return CSVConverter()
-        elif format == Format.json:
-            return JSONConverter()
-        elif format == Format.toml:
-            return TOMLConverter()
-        elif format == Format.yaml:
-            return YAMLConverter()
-        else:
+        converters = {
+            Format.json: JSONConverter,
+            Format.toml: TOMLConverter,
+            Format.yaml: YAMLConverter,
+        }
+        converter = converters[format]()
+        if not converter:
             raise ValueError(f"Unknown format {format}")
+        return converter
 
 
 class JSONConverter(Converter):
-    def load(self, path: Path) -> object:
-        if path == Path("-"):
-            return json.load(sys.stdin)
-        else:
-            with open(path) as f:
-                return json.load(f)
+    def load(self, file: io.IOBase) -> object:
+        return json.load(file)
 
-    def dump(self, path: Path, data: object):
-        if path == Path("-"):
-            json.dump(data, sys.stdout)
-        else:
-            with open(path, "w") as f:
-                json.dump(data, f)
+    def dump(self, file: io.IOBase, data: object):
+        json.dump(data, file)
 
 
 class YAMLConverter(Converter):
-    def load(self, path: Path) -> object:
-        if path == Path("-"):
-            return yaml.load(sys.stdin, Loader=yaml.FullLoader)
-        else:
-            with open(path) as f:
-                return yaml.load(f, Loader=yaml.FullLoader)
+    def load(self, file: io.IOBase) -> object:
+        return yaml.load(file, Loader=yaml.FullLoader)
 
-    def dump(self, path: Path, data: object):
-        if path == Path("-"):
-            yaml.dump(data, sys.stdout)
-        else:
-            with open(path, "w") as f:
-                yaml.dump(data, f)
+    def dump(self, file: io.IOBase, data: object):
+        yaml.dump(data, file)
 
 
 class TOMLConverter(Converter):
-    def load(self, path: Path) -> object:
-        if path == Path("-"):
-            return toml.load(sys.stdin)
-        else:
-            with open(path) as f:
-                return toml.load(f)
+    def load(self, file: io.IOBase) -> object:
+        return toml.load(file)
 
-    def dump(self, path: Path, data: object):
-        if path == Path("-"):
-            toml.dump(data, sys.stdout)
-        else:
-            with open(path, "w") as f:
-                toml.dump(data, f)
+    def dump(self, file: io.IOBase, data: object):
+        toml.dump(data, file)
 
 
 @app.command()
@@ -112,7 +89,9 @@ def convert(
             allow_dash=True,
         ),
     ] = Path("-"),
-    output: Annotated[Optional[Path], typer.Option("--output", "-o")] = Path("-"),
+    output: Annotated[
+        Optional[Path], typer.Option("--output", "-o", allow_dash=True)
+    ] = Path("-"),
     input_format: Annotated[Format, typer.Option("--input-format", "-F")] = None,
     output_format: Annotated[Format, typer.Option("--output-format", "-f")] = None,
 ):
@@ -126,13 +105,55 @@ def convert(
             raise ValueError("Could not determine output format")
     loader = Converter.converter(input_format)
     dumper = Converter.converter(output_format)
-    data = loader.load(input)
-    dumper.dump(output, data)
+
+    if input == Path("-"):
+        input_file = sys.stdin
+    else:
+        input_file = open(input, "r")
+
+    if output == Path("-"):
+        output_file = sys.stdout
+    else:
+        output_file = open(output, "w")
+
+    data = loader.load(input_file)
+    dumper.dump(output_file, data)
 
 
 @app.command()
 def formats():
     print(", ".join([e.value for e in Format]))
+
+
+@app.command()
+def get_key(
+    input: Annotated[
+        Optional[Path],
+        typer.Option(
+            "--input",
+            "-i",
+            file_okay=True,
+            dir_okay=False,
+            readable=True,
+            allow_dash=True,
+        ),
+    ] = Path("-"),
+    input_format: Annotated[Format, typer.Option("--input-format", "-F")] = None,
+    key: Annotated[str, typer.Argument(...)] = None,
+):
+    if input_format is None:
+        input_format = Format.format_for_path(input)
+        if input_format is None:
+            raise ValueError("Could not determine input format")
+    loader = Converter.converter(input_format)
+
+    if input == Path("-"):
+        input_file = sys.stdin
+    else:
+        input_file = open(input, "r")
+
+    data = loader.load(input_file)
+    print(data[key])
 
 
 if __name__ == "__main__":
