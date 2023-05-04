860c0bcd3fcd12e6812b3d2241838d024c488e9f
Jonathan Wight <jwight@mac.com>
2023-05-04 11:11:04 -0700

    More unit tests.

--- transmogrify/test_main.py
+++ transmogrify/test_main.py
@@ -1,12 +1,27 @@
 from typer.testing import CliRunner
-
 from .main import app
+import shlex
+from pathlib import Path
 
 runner = CliRunner()
 
 
-def test_app():
-    result = runner.invoke(app, ["formats"])
+def test_formats():
+    result = runner.invoke(app, shlex.split("formats"))
     assert result.exit_code == 0
     assert "json" in result.stdout
     assert "xml" not in result.stdout
+
+
+def test_convert_json_to_toml():
+    result = runner.invoke(
+        app, shlex.split("convert --input tests/test.json --output-format toml")
+    )
+    assert result.exit_code == 0
+    assert result.stdout == Path("tests/test.toml").read_text()
+
+
+def test_convert_get_key():
+    result = runner.invoke(app, shlex.split("get-key --input tests/test.json hello"))
+    assert result.exit_code == 0
+    assert "world" in result.stdout
